b9ac78021fe61e904bbdd7d21d436c783e5d1916
Rodrigo Moura Rodrigues <rodrigo.rodrigues@s2it.com.br>
2018-10-09 13:01:28 -0300

    Implementado opcao de menu de Pesquisar Doacoes/Necessidades

---
+++ SantaHelena.ClickDoBem.BackOffice/Controllers/ItensController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SantaHelena.ClickDoBem.BackOffice.Models;
+
+namespace SantaHelena.ClickDoBem.BackOffice.Controllers
+{
+    public class ItensController : Controller
+    {
+        public IActionResult Pesquisar()
+        {
+            return View();
+        }
+
+    }
+}
