0211a5d55e8260faa1e64f1af82a92c440273a64
Rodrigo Moura Rodrigues <rodrigo.rodrigues@s2it.com.br>
2018-10-10 09:48:43 -0300

    Inicio da geracao de tela de pesquisa de itens

--- SantaHelena.ClickDoBem.BackOffice/Startup.cs
+++ SantaHelena.ClickDoBem.BackOffice/Startup.cs
@@ -83,6 +83,7 @@ namespace SantaHelena.ClickDoBem.BackOffice
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
